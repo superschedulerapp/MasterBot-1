ad38a2bca774cebaf4bbd1f4056eb70dadac00bd
ichaleynbin <ichaleynbin@gmail.com>
2013-11-19 15:02:31 -0500

    deleted:    MasterBot/Code/BotShell.py~ 	deleted:    MasterBot/Code/BotTalking.py 	deleted:    MasterBot/Code/BotTalking.pyc 	deleted:    MasterBot/Code/BotTalking.py~ 	deleted:    MasterBot/Code/MasterBotter.py 	deleted:    MasterBot/Code/MasterBotter.pyc 	deleted:    MasterBot/Code/MasterBotter.py~ 	deleted:    MasterBot/Code/Runcode.py 	deleted:    MasterBot/Code/Runcode.pyc deleted:    MasterBot/Code/Runcode.py~ 	deleted:    MasterBot/Code/__init__.py 	deleted:    MasterBot/Code/__init__.pyc 	deleted:    MasterBot/Code/commandant.py 	deleted:    MasterBot/Code/commandant.pyc 	deleted:    MasterBot/Code/commandant.py~ 	deleted:    MasterBot/Code/importData.py 	deleted:    MasterBot/Code/importData.pyc 	deleted:    MasterBot/Code/importData.py~ 	deleted:    MasterBot/Code/zipIt.py 	deleted:    MasterBot/Code/zipIt.pyc 	deleted:    MasterBot/Code/zipIt.py~ 	deleted:    MasterBot/NewData/BotsPickle 	deleted:    MasterBot/NewData/ChannelsPickle 	deleted:    MasterBot/NewData/Data.txt 	deleted:    MasterBot/NewData/Data.txt~ 	deleted:    MasterBot/NewData/DevsPickle 	deleted:    MasterBot/NewData/FriendsPickle 	deleted:    MasterBot/NewData/LinksPickle 	deleted:    MasterBot/NewData/OpsPickle 	deleted:    MasterBot/NewData/OwnersPickle 	deleted:    MasterBot/NewData/suggestions.txt

--- MasterBot/Code/importData.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def initData():
-    if not os.path.isdir('./UserData/'):
-        os.path.mkdir('./UserData/')
-    if not os.path.isfile('./UserData/Data.txt'):
-        os.system('cp ./NewData/*.* ./UserData/\n')
